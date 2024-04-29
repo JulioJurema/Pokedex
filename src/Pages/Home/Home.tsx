6afdddce214b92a31436af5a96786a3f3f7b6d42
juliojurema <zueranever3j@gmail.com>
2024-04-28 23:04:08 -0300

    Upload

---
+++ src/Pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+
+const HomePage = () =>{
+    return (
+        <>
+            <div>
+                <h1 className=" text-orange-600 font-thin text-center">olá mundo!!</h1>
+            </div>
+        </>
+    )
+}
+
+export default HomePage;
